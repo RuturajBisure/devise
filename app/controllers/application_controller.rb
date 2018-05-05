fda97ff552f346682b5a574c7761d2f93ab05333
RuturajBisure <ruturaj.bisure@gmail.com>
2018-05-05 17:13:43 +0530

    blog login avatar and validation added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:firstname, :lastname, :email, :password, :avatar])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username,:firstname, :lastname, :email, :password, :avatar])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:firstname, :lastname, :email, :password, :avatar, :phone_number])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username,:firstname, :lastname, :email, :password, :avatar, :phone_number])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates :password , :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[0-9]).{8,}\z/, message: "must be at least 8 characters and include one number and one letter."}
+  validates :username, :firstname, :lastname, :phone_number, :presence => true
+  validates_format_of :phone_number, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
+  validates :phone_number, :numericality => true, :length => { :minimum => 10, :maximum => 11 }
+  
 end
+
